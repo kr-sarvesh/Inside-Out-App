591ad2c36fb9a144a7d081a992d093d543de93d4
kr-sarvesh <kr.sr.rssb@gmail.com>
2021-09-04 21:59:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xrsdfo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xrsdfo)
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './style.css';
+import { useState } from 'react';
+const emojiDictionary = {
+  '😀': 'smiling',
+  '😥': 'sad',
+  '😲': 'disbelief',
+  '😒': 'annoyance',
+  '❤️': 'love',
+  '🥡': 'takeout-box'
+};
+
+const msg = 'INSIDE-OUT';
+const emojisWeknow = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState('');
+
+  var emojiInputHandler = event => {
+    const userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = 'we dont have this in database';
+    }
+    setMeaning(meaning);
+  };
+
+  var emojiCLickHandler = emoji => {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  };
+  return (
+    <div className="App">
+      <h1> {msg} </h1>
+      <input onChange={emojiInputHandler} />
+      <h2>{meaning}</h2>
+      <h3>Emojis we know</h3>
+      {emojisWeknow.map(function(emoji) {
+        return (
+          <span
+            onClick={() => emojiCLickHandler(emoji)}
+            style={{
+              cursor: 'pointer',
+              fontSize: '45px'
+            }}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
